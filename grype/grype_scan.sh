f2fd9fddc675dd9dbd158804d97a4332fe4a539a
Chan Young Lee <cyl0504@gmail.com>
2025-06-24 22:47:02 +0900

    Update: grype_scan.py

--- grype/grype_scan.sh
+++ grype/grype_scan.sh
@@ -8,9 +8,9 @@ curl -sSfL https://raw.githubusercontent.com/anchore/grype/main/install.sh | sh 
 # Grype 버전 확인
 grype version
 
-# 현재 디렉토리 기준 tar 파일이 있을 것으로 가정
-echo "[+] Scanning local tar image: python_3.11.tar"
-grype tar:python_3.11.tar -o table > scan_external.txt
+# /image 디렉토리에 있는 tar 파일 스캔
+echo "[+] Scanning local tar image: /image/python_3.11.tar"
+grype tar:/image/python_3.11.tar -o table > scan_external.txt
 
 # 결과 출력
 echo -e "\n=== Scan Result ==="
